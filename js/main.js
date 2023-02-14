eed65f4a70f8586d9fffe0075a8d53a04aa4b7b2
Mariana Lobo <m.lobocasas@gmail.com>
2023-02-14 10:07:14 +0100

    add round counter

--- js/main.js
+++ js/main.js
@@ -7,26 +7,37 @@ const btnGame = document.querySelector('.js-btn');
 const userClues = document.querySelector('.js-clues');
 const numberTries = document.querySelector('.js-tries');
 
+let round = 0;
 
 //GENERAR NÚMERO ALEATORIO
 
 const computerNumber = Math.ceil(Math.random() * 100);
 console.log(computerNumber);
 
+// COMPARAR
 function compare() {
     const userNumber = parseInt(inputNumber.value);
-    if (userNumber > computerNumber) {
+    if (userNumber > 100 || userNumber < 1) {
+        userClues.innerHTML = "El número debe estar entre 1 y 100";
+    }else if (userNumber > computerNumber) {
         userClues.innerHTML = "Demasiado alto";
     } else if (userNumber < computerNumber) {
         userClues.innerHTML = "Demasiado bajo";
     } else if (userNumber === computerNumber) {
-        userClues.innerHTML = "Has ganado campeona!!!";
+        userClues.innerHTML = "Has ganado campeona!!!";    
     }
+}  
+ 
+//CONTADOR DE INTENTOS
+function counterRound(){
+    round++;
+    numberTries.innerHTML = "Número de intentos:" + round;
 }
 
 //EVENTO
 function handleClick(event) {
     event.preventDefault(); 
     compare();
+    counterRound();
 }
 btnGame.addEventListener('click', handleClick);
